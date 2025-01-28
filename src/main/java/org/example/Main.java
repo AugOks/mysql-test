f990f0325c3a973ecd58d38543c36803c7371694
augoks <augustdo@stud.ntnu.no>
2025-01-28 11:49:03 +0100

    fixed pom and changed main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
-
+import java.sql.*;
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
+
+    public static void main(String args[]){
+      try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con=DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/exc","ado","google");
+//here sonoo is database name, root is username and password
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery("select studname from students");
+        while(rs.next())
+          System.out.println(rs.getString(1));
+        con.close();
+      }catch(Exception e){ System.out.println(e);}
+    }
   }
-}

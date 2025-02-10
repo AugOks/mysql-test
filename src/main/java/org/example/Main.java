7885b9ab89519871bacd5bae7dd468447406dff2
augoks <augustdo@stud.ntnu.no>
2025-02-10 14:47:44 +0100

    fixed try with resources

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,17 @@ package org.example;
 import java.sql.*;
 public class Main {
 
-    public static void main(String args[]){
-      try{
+    public static void main(String[] args){
+      try(Connection con=DriverManager.getConnection(
+          "jdbc:mysql://localhost:3306/exc","ado","google")){
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con=DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/exc","ado","google");
 //here sonoo is database name, root is username and password
-        Statement stmt=con.createStatement();
-        ResultSet rs=stmt.executeQuery("select studname from students");
+        ResultSet rs;
+        try (Statement stmt = con.createStatement()) {
+          rs = stmt.executeQuery("select studname from students");
+        }
         while(rs.next())
           System.out.println(rs.getString(1));
-        con.close();
       }catch(Exception e){ System.out.println(e);}
     }
   }

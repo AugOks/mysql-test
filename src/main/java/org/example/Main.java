4f8e6f5b26221d44d5590aab474ed06de71a8b9e
augoks <augustdo@stud.ntnu.no>
2025-02-10 15:13:53 +0100

    added some other queries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,20 @@ public class Main {
       try(Connection con=DriverManager.getConnection(
           "jdbc:mysql://localhost:3306/exc","ado","google")){
         Class.forName("com.mysql.cj.jdbc.Driver");
-//here sonoo is database name, root is username and password
+//here exc is database name, root is username and password
         ResultSet rs;
         try (Statement stmt = con.createStatement()) {
           rs = stmt.executeQuery("select studname from students");
+          while(rs.next())
+            System.out.println(rs.getString(1));
+        //  int insert = stmt.executeUpdate("INSERT into  students values( 4, 'jan')");
+          //System.out.println(insert);
+          ResultSet get;
+          get = stmt.executeQuery("select studname from students where studname like 'jan'");
+          while(get.next()) {
+            System.out.println("\n" + get.getString(1));
+          }
         }
-        while(rs.next())
-          System.out.println(rs.getString(1));
       }catch(Exception e){ System.out.println(e);}
     }
   }
